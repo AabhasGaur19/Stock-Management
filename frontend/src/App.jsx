2e8c2b3d603be3934233e51d278e1cfd92178882
AabhasGaur19 <aabhasgaur14@gmai8l.com>
2025-03-06 21:04:34 +0530

    here we go again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,3 @@
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import LoginPage from "./screens/LoginPage";
-// import SignupPage from "./screens/SignupPage";
-// import Dashboard from "./screens/dashboard";
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/login" element={<LoginPage />} />
-//         <Route path="/signup" element={<SignupPage />} />
-//         <Route path="/dashboard" element={<Dashboard />} />
-
-//       </Routes>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./screens/LoginPage";
@@ -40,4 +19,3 @@ const App = () => {
 };
 
 export default App;
-
